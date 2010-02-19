b7496c61e4416de3a1e819800dd1e3ee3d5ece81
jfr1982 <jfr1982@02d05946-1425-11df-9240-ad675aeae82e>
2010-02-19 20:03:51 +0000

    # Modificada la solución de vs2010 a vs2008, vs2010 es muy pesado para mi GB de ram

---
+++ trunk/NLogMonitor/AppConfiguration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace LogMonitor
+{
+    class AppConfiguration : ConfigurationSection
+    {
+        static public AppConfiguration Instance = (AppConfiguration) ConfigurationManager.GetSection("AppConfiguration");
+
+        [ConfigurationProperty("Directory", IsRequired = true)]
+        public String Directory
+        {
+            get { return (String)this["Directory"]; }
+        }
+    }
+
+    class SmtpConfiguration : ConfigurationSection
+    {
+        static public SmtpConfiguration Instance = (SmtpConfiguration)ConfigurationManager.GetSection("SmtpConfiguration");
+
+        [ConfigurationProperty("Host", IsRequired = true)]
+        public String Host
+        {
+            get { return (String)this["Host"]; }
+        }
+
+        [ConfigurationProperty("Port", IsRequired = true)]
+        public int Port
+        {
+            get { return (int)this["Port"]; }
+        }
+
+        [ConfigurationProperty("User", IsRequired = true)]
+        public String User
+        {
+            get { return (String)this["User"]; }
+        }
+
+        [ConfigurationProperty("Pass", IsRequired = true)]
+        public String Pass
+        {
+            get { return (String)this["Pass"]; }
+        }
+    }
+
+    class MailConfiguration : ConfigurationSection
+    {
+        static public MailConfiguration Instance = (MailConfiguration)ConfigurationManager.GetSection("MailConfiguration");
+
+        [ConfigurationProperty("From", IsRequired = true)]
+        public String From
+        {
+            get { return (String)this["From"]; }
+        }
+
+        [ConfigurationProperty("To", IsRequired = true)]
+        public int To
+        {
+            get { return (int)this["To"]; }
+        }       
+    }
+}
--- trunk/NLogMonitor/Program.cs
+++ trunk/NLogMonitor/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.IO;
-using System.Data.SQLite;
-using System.Data.Common;
-using System.Data;
 using System.Text.RegularExpressions;
 using System.Net.Mail;
 using System.Net;
