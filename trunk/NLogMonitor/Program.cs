db3ca4826bc653d51b33540192b4ab633b64c30f
jfr1982 <jfr1982@02d05946-1425-11df-9240-ad675aeae82e>
2010-03-05 14:10:54 +0000

    # Modificado para que la expresion regular este compilada

--- trunk/NLogMonitor/Program.cs
+++ trunk/NLogMonitor/Program.cs
@@ -17,20 +17,27 @@ namespace LogMonitor
             {
                 string file_content = File.ReadAllText(file_name);
                 StringBuilder mail_content = new StringBuilder();
-                mail_content.Append(process_content(".*LM_ERROR.*", file_name, file_content)); // procesamos errores
-                mail_content.Append(process_content(".*LM_WARNING.*", file_name, file_content)); // procesamos errores
+                Console.WriteLine("Procesando " + file_name + "...");
+                mail_content.Append(process_content(".*LM_ERROR.*|.*LM_WARNING.*", file_name, file_content)); // procesamos errores
+                Console.WriteLine(file_name + " procesado");
+                
                 if (mail_content.Length != 0)   // si el fichero ha tenido algun error
                 {
-                    send_email(mail_content.ToString());   
+                    Console.WriteLine("Enviando correo ...");
+                    send_email(file_name, mail_content.ToString());
+                    Console.WriteLine("Correo enviado");
                 }               
             }
         }
 
         static String process_content(string pattern, string file_name, string file_content)
         {
-            Regex reg = new Regex(pattern);
+            Regex reg = new Regex(pattern, RegexOptions.Compiled);
 
+            DateTime t = DateTime.Now;
             MatchCollection mc = reg.Matches(file_content);
+            Console.WriteLine("{0} matches found. Tardo {1} segundos", mc.Count, DateTime.Now - t);
+
             StringBuilder line_matched = new StringBuilder();
             foreach (Match m in mc)
             {
@@ -40,16 +47,22 @@ namespace LogMonitor
             return line_matched.ToString();
         }
 
-        static void send_email(String mail_content)
+        static void send_email(String file_name, String mail_content)
         {
             MailAddress ma_from = new MailAddress(MailConfiguration.Instance.From);
 
             MailMessage m = new MailMessage(ma_from, ma_from);
             m.Body = mail_content;
+            m.Subject = "Hay errores en el fichero " + file_name;
 
             SmtpClient client = new SmtpClient(SmtpConfiguration.Instance.Host, SmtpConfiguration.Instance.Port);
-            client.EnableSsl = true;
-            client.Credentials = new NetworkCredential(SmtpConfiguration.Instance.User, SmtpConfiguration.Instance.Pass);
+            client.EnableSsl = SmtpConfiguration.Instance.EnableSsl;
+            
+            if (client.EnableSsl)
+            {
+                client.Credentials = new NetworkCredential(SmtpConfiguration.Instance.User, SmtpConfiguration.Instance.Pass);
+            }
+
             client.Send(m);
         }
     }
